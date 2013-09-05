79eb55ed2a6ec32d57e5125f9eb9a56247eb23c6
sebastien <sebastien@vaio-vpcf1>
2013-09-05 15:10:52 +0200

    Updated

---
+++ public/themes/openstore/js/main.js
@@ -0,0 +1,28 @@
+/** 
+ * Sidebar navigation
+ */
+
+
+/**
+ * jQuery overrides
+ */
+jQuery.redirect = function(url, params) {
+		url = url || window.location.href || '';
+		url = url.match(/\?/) ? url : url + '?';
+
+		for (var key in params) {
+			var re = RegExp(';?' + key + '=?[^&;]*', 'g');
+			url = url.replace(re, '');
+			url += ';' + key + '=' + params[key];
+		}
+		// cleanup url 
+		url = url.replace(/[;&]$/, '');
+		url = url.replace(/\?[;&]/, '?');
+		url = url.replace(/[;&]{2}/g, ';');
+		// $(location).attr('href', url);
+		window.location.replace(url);
+	};
+
+
+
+
