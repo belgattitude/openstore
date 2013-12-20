0cb27f6bc93202c1cac35cd11c7b278909f0beaa
sebastien <belgattitude@gmail.com>
2013-12-20 10:48:02 +0100

    Fixed var_dump

--- module/Akilia/src/Akilia/Synchronizer.php
+++ module/Akilia/src/Akilia/Synchronizer.php
@@ -648,8 +648,6 @@ NULL , '2', '3521', '1', NULL , NULL , NULL , NULL , NULL , NULL
 
 		foreach($elements as $key => $element) {
 			$akilia1Db = $element['akilia1db'];
-			var_dump($element);
-			die();
 			if ($element['pricelist'] != '') {
 				$pricelist_clause = "and t.id_pays = '" . $element['pricelist'] . "'";
 			} else {
