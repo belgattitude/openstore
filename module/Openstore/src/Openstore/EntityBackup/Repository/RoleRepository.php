eecc74a8060f8abf7a3f829056a3ce2c9e762cb2
Sébastien Vanvelthem <belgattitude@gmail.com>
2015-12-22 14:11:09 +0100

    Removed old entities

--- module/Openstore/src/Openstore/EntityBackup/Repository/RoleRepository.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace OpenstoreSchema\Core\Entity\Repository;
-
-use Gedmo\Tree\Entity\Repository\NestedTreeRepository;
-
-class RoleRepository extends NestedTreeRepository
-{
-    // your code here
-}
